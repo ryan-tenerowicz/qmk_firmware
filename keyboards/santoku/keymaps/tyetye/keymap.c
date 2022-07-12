49fd5cc32fee9092779fd17030b559157f44c122
Shay Merrill <shaymdev@gmail.com>
2022-07-12 12:08:22 -0600

    Remove unneeded comment

--- keyboards/santoku/keymaps/tyetye/keymap.c
+++ keyboards/santoku/keymaps/tyetye/keymap.c
@@ -98,7 +98,7 @@ static bool in_alttab = false; // does an ALT-TAB, for windows cycling, without 
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_QWERTY] = { /*QWERTY*/
+	[_QWERTY] = { 
 		{KC_TAB,              KC_Q,              KC_W,     KC_E,    KC_R,     KC_T,   KC_Y,         KC_U,       KC_I,    KC_O,   KC_P,                  KC_BSLS },
 		{MT(MOD_LGUI,KC_ESC), MT(MOD_LCTL,KC_A), KC_S,     KC_D,    KC_F,     KC_G,   KC_H,         KC_J,       KC_K,    KC_L,   MT(MOD_RCTL,KC_SCLN),  MT(MOD_RGUI,KC_QUOT)},
 		{KC_LSFT,             MT(MOD_LALT,KC_Z), KC_X,     KC_C,    KC_V,     KC_B,   KC_N,         KC_M,       KC_COMM, KC_DOT, MT(MOD_RALT,KC_SLSH),  KC_RSFT },
