5fd3b7aff0ac427de9e66216ab22710c38b26ec0
Ryan <github@ryantenerowicz.dev>
2023-11-28 02:07:56 -0800

    working single split trackpoint

--- keyboards/rtenerowicz/rev1/keymaps/default/keymap.c
+++ keyboards/rtenerowicz/rev1/keymaps/default/keymap.c
@@ -16,11 +16,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_numpad_5x4( /* Base */
-    KC_A,   KC_B,    KC_C, KC_D,
-    KC_E,   KC_F,    KC_G,
-    KC_H,   KC_I,    KC_J, KC_K,
-    KC_L,   KC_M,    KC_N,
-    KC_O,   KC_P,    KC_Q
+  [0] = LAYOUT(
+    KC_L,   KC_R
   ),
 };
