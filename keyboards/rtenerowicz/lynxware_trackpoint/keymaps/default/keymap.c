2f2002338bc1e24f21c678bcbf0cac2c55785652
Ryan <github@ryantenerowicz.dev>
2024-04-17 23:42:35 -0700

    matrix

--- keyboards/rtenerowicz/lynxware_trackpoint/keymaps/default/keymap.c
+++ keyboards/rtenerowicz/lynxware_trackpoint/keymaps/default/keymap.c
@@ -39,11 +39,20 @@ enum {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT(
-   
+    KC_0, KC_1, KC_2, KC_3, KC_4, KC_5,
+    KC_0, KC_1, KC_2, KC_3, KC_4, KC_5,
+    KC_0, KC_1, KC_2, KC_3, KC_4, KC_5,
+    KC_0, KC_1, KC_2, KC_3, KC_4, KC_5,
+
     KC_1, KC_2, KC_3, 
     KC_5, KC_6, KC_7, 
     KC_9, KC_0, KC_A,
     KC_9, KC_0, KC_A,
+
+    KC_0, KC_1, KC_2, KC_3, KC_4, KC_5,
+    KC_0, KC_1, KC_2, KC_3, KC_4, KC_5,
+    KC_0, KC_1, KC_2, KC_3, KC_4, KC_5,
+    KC_0, KC_1, KC_2, KC_3, KC_4, KC_5,
 
     KC_1, KC_1, KC_1,
     KC_1, KC_1, KC_1, 
